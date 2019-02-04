3b82da3d208d332ab264a1b0f0093f8663222d7d
kuyanov <43725417+kuyanov@users.noreply.github.com>
2019-02-04 18:31:31 +0300

    Exam settings page - combobox

--- teacher/exam_settings.py
+++ teacher/exam_settings.py
@@ -30,15 +30,26 @@ class ExamSettings(ExamWidgetBase):
         duration_input = Qt.QLineEdit(self.exam_info['duration'])
         duration_input.setFont(Qt.QFont('Arial', 20))
 
+        state_title = Qt.QLabel('Для участия:')
+        state_title.setFont(Qt.QFont('Arial', 20))
+
+        state_box = Qt.QComboBox()
+        state_box.setFont(Qt.QFont('Arial', 20))
+        state_box.addItems(['Недоступен', 'Открыт'])
+
         title_layout = Qt.QVBoxLayout()
         title_layout.addWidget(name_title)
         title_layout.addSpacerItem(Qt.QSpacerItem(0, 20))
         title_layout.addWidget(duration_title)
+        title_layout.addSpacerItem(Qt.QSpacerItem(0, 20))
+        title_layout.addWidget(state_title)
 
         input_layout = Qt.QVBoxLayout()
         input_layout.addWidget(name_input)
         input_layout.addSpacerItem(Qt.QSpacerItem(0, 20))
         input_layout.addWidget(duration_input)
+        input_layout.addSpacerItem(Qt.QSpacerItem(0, 20))
+        input_layout.addWidget(state_box)
 
         main_layout = Qt.QHBoxLayout()
         main_layout.addSpacerItem(Qt.QSpacerItem(20, 0))
